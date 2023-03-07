e0c63b51f3219308fbbbc95eaab10bd6777e3db6
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2023-03-07 20:43:24 +0530

    static GIF generator

---
+++ static_gif_gen.py
@@ -0,0 +1,23 @@
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+import imageio
+
+# Set up variables
+resolution = input("Enter the resolution (e.g. 1920x1080): ")
+fps = int(input("Enter the frames per second (e.g. 60): "))
+duration = int(input("Enter the duration in seconds (e.g. 10): "))
+
+# Split resolution into width and height
+width, height = map(int, resolution.split("x"))
+
+# Generate static frames
+frames = []
+for i in tqdm(range(int(fps * duration)), desc="Generating frames"):
+    frame = np.random.randint(0, 256, (height, width, 3), dtype=np.uint8)
+    frames.append(Image.fromarray(frame))
+
+# Save frames as GIF
+filename = f"static_{resolution}.gif"
+imageio.mimsave(filename, frames, fps=fps)
+print(f"Static saved as {filename}")
