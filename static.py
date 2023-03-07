9d1e7ac342cd49ffb7cb5c635c529871617f8ddf
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2023-03-07 17:15:30 +0530

    Update static.py

--- static.py
+++ static.py
@@ -2,18 +2,18 @@ import cv2
 import numpy as np
 
 # Set the width and height of the video
-width = 640
-height = 480
+width = 1920
+height = 1080
 
 # Set the frame rate of the video
-fps = 30
+fps = 60
 
 # Create a VideoWriter object to write the output video
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('static.mp4', fourcc, fps, (width, height))
 
 # Generate frames of static and write them to the output video
-for i in range(300):
+for i in range(1800):
     # Create a random matrix of values between 0 and 255
     static = np.random.randint(0, 256, (height, width, 3), dtype=np.uint8)
 
