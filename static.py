576744b63bc791084f9b01e6f7b0024d5eae7f90
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2023-03-07 17:14:33 +0530

    main script

---
+++ static.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+# Set the width and height of the video
+width = 640
+height = 480
+
+# Set the frame rate of the video
+fps = 30
+
+# Create a VideoWriter object to write the output video
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('static.mp4', fourcc, fps, (width, height))
+
+# Generate frames of static and write them to the output video
+for i in range(300):
+    # Create a random matrix of values between 0 and 255
+    static = np.random.randint(0, 256, (height, width, 3), dtype=np.uint8)
+
+    # Apply a Gaussian blur to the matrix to simulate the blurring effect
+    static = cv2.GaussianBlur(static, (3, 3), 0)
+
+    # Write the frame to the output video
+    out.write(static)
+
+# Release the VideoWriter object
+out.release()
